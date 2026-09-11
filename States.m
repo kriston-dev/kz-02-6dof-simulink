e6f8bcad1d1e2d021054494d74ead32af8ae5366
Kriston Rickman <kristonrickman@gmail.com>
2026-09-11 08:31:52 -0700

    v1_01

---
+++ States.m
@@ -0,0 +1,18 @@
+%Created by Kriston Rickman
+%Data created: 09/11/26
+%v1_01 for kz_02_6dof_sim v1_01
+%KZ-02 6DOF simulation States
+%Notes:Will also give general idea of this
+
+classdef States
+    enumeration
+        MAIN_MENU
+        USER_AIRCRAFT_OPT
+        USER_SAVED_AIRCRAFT_MENU
+        EDIT_USER_AIRCRAFT_DATA
+        DISP_USER_AIRCRAFT_DATA
+        INPUT_MENU
+        MANUAL_CONTROL_SIM
+        AUTOPILOT_SIM
+    end
+end
